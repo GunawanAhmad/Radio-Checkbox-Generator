99e428bdaa7059875dd2fdbbad019fa8268a494f
Gunawan <gunawanahmadef@gmail.com>
2021-01-24 19:15:31 +0700

    finish desktop design but not complete yet

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let colorInput = document.querySelector("input[type='color']");
-let radio = document.querySelector(".raio-container .fake-radio");
+// let colorInput = document.querySelector("input[type='color']");
+// let radio = document.querySelector(".raio-container .fake-radio");
 
-let colorVar = getComputedStyle(document.body).getPropertyValue("--black");
+// let colorVar = getComputedStyle(document.body).getPropertyValue("--black");
 
-colorInput.addEventListener("change", () => {
-  document.documentElement.style.setProperty("--black", colorInput.value);
+// colorInput.addEventListener("change", () => {
+//   document.documentElement.style.setProperty("--black", colorInput.value);
+// });
+
+let settingsTab = document.querySelectorAll(".editor ul li .edit-title");
+
+settingsTab.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tab.parentElement.classList.toggle("show");
+  });
 });
